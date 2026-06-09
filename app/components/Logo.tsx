cf1a6784c361880b6792526719c200a3aa4a7d90
graciadavid <gracia.david@gmail.com>
2026-06-09 18:08:14 +0200

    add Logo component

---
+++ app/components/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+ return (
+   <span className="text-gray-800 text-xs md:text-sm font-light tracking-widest uppercase">
+     Wiki<span className="font-bold">Seconds</span>
+   </span>
+ )
+}
