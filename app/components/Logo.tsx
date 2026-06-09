70423bc167945bfd1bde2a5771edf7c3b51770f6
graciadavid <gracia.david@gmail.com>
2026-06-09 18:20:10 +0200

    cleanup unused components

--- app/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-export default function Logo() {
- return (
-   <span className="text-gray-800 text-xs md:text-sm font-light tracking-widest uppercase">
-     Wiki<span className="font-bold">Seconds</span>
-   </span>
- )
-}
